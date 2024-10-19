869f59a3284f9d59e416fc93d83b45e8cb0672d9
AyushPatel90 <A.P.6306390280@GMAIL.COM>
2024-10-19 12:10:09 +0530

    ecom

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+import { Toaster } from 'react-hot-toast';
+
+function Layout({children}) {
+  return (
+    <div>
+        <Header></Header>
+        <main style={{minHeight:"70vh"}}>
+          <Toaster/>
+          {children}</main>
+        <Footer></Footer>
+    </div>
+  )
+}
+
+export default Layout
---
+++ client/src/pages/Policy.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Layout from "./../components/Layout/Layout";
+
+const Policy = () => {
+  return (
+    <Layout>
+      <div className="row contactus ">
+        <div className="col-md-6 ">
+          <img
+            src="/images/contact.jpg"
+            alt="contactus"
+            style={{ width: "100%", padding:"0px 0px 0px 100px"}}
+          />
+        </div>
+        <div className="col-md-4">
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+          <p>add privacy policy</p>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Policy;
